08095f7f39b5419c13a2a47182303fd467944b4d
Mateusz <zatwarnicki.mateusz@gmail.com>
2023-03-11 10:50:52 +0100

    Next Update

--- JS/main.js
+++ JS/main.js
@@ -4,21 +4,28 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1600;
 canvas.height = 900;
 
+
+
+//Instances:
+const player = new Player ({
+    positionX: 100,
+    positionY:100,
+    width:100,
+    height:150});
+
 //Animation loop:
 let lasttime;
-// function animate() {
-//     if (lasttime != null){
-//         let delta = lasttime -Date.now;
+ function animate() {
+     if (lasttime != null){
+         let delta = (Date.now() - lasttime)/2;
+         //Update section:
+        ctx.clearRect(0,0,canvas.width,canvas.height)
+        player.update(delta);
+        
+         
+     }
+     lasttime = Date.now();
+     window.requestAnimationFrame(animate);
+ }
 
-//         window.requestAnimationFrame(animate);
-//         console.log(delta);
-//     }
-//     lasttime = Date.now;
-// }
-
-function test(){
-    let x = 1;
-    console.log(x);
-}
-
-animate();
+ window.requestAnimationFrame(animate);

aaab66bd72cb01357525e0e176d5002259112045
matzat90 <100511658+matzat90@users.noreply.github.com>
2023-03-11 09:53:10 +0100

    First Update

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// Canvas settings:
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 1600;
+canvas.height = 900;
+
+//Animation loop:
+let lasttime;
+animate(){
+    
+}
